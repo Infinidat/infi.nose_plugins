ea94f7acb13b56f75661a87f74457d8ec4924b66
Guy Rozendorn <guy@rzn.co.il>
2015-03-04 10:45:24 +0200

    HOSTDEV-1978 LogbookPlugin: does not enable logbook compatibility if gevent not present

--- src/infi/nose_plugins/logbook.py
+++ src/infi/nose_plugins/logbook.py
@@ -14,12 +14,23 @@ class LogbookPlugin(Plugin):  # pragma: no cover
     def help(self):
         return 'logbook compatability'
 
-    def begin(self):
+    def _send_logbook_records_to_standard_logging(self):
         try:
             from logbook.compat import LoggingHandler
-            from logbook.concurrency import enable_gevent
         except ImportError:
             pass
         else:
             LoggingHandler().push_application()
+
+    def _enable_logbook_gevent(self):
+        try:
+            from logbook.concurrency import enable_gevent
+        except ImportError:
+            pass
+        else:
             enable_gevent()
+
+    def begin(self):
+        self._enable_logbook_gevent()
+        self._send_logbook_records_to_standard_logging()
+
